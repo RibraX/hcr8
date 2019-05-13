1bb42dd18e099be3d289d238848290e5075e67b9
ribrax <joaobraz@ribrax.com.br>
2019-05-13 11:00:35 -0300

    type_nf_payment

--- l10n-brazil/l10n_br_account_product/models/account_invoice.py
+++ l10n-brazil/l10n_br_account_product/models/account_invoice.py
@@ -391,25 +391,6 @@ class AccountInvoice(models.Model):
         compute='_compute_amount')
     payment_mode_id = fields.Many2one(
         comodel_name='payment.mode', string="Payment Mode")
-    type_nf_payment = fields.Selection([
-        ('01', u'01 - Dinheiro'),
-        ('02', u'02 - Cheque'),
-        ('03', u'03 - Cartão de Crédito'),
-        ('04', u'04 - Cartão de Débito'),
-        ('06', u'05 - Crédito Loja'),
-        ('10', u'10 - Vale Alimentação'),
-        ('11', u'11 - Vale Refeição'),
-        ('12', u'12 - Vale Presente'),
-        ('13', u'13 - Vale Combustível'),
-        ('15', u'15 - Boleto Bancário'),
-        ('90', u'90 - Sem pagamento'),
-        ('99', u'99 - Outros')
-    ], string='Tipo de Pagamento da NF',
-        help=u'Obrigatório o preenchimento do Grupo Informações de Pagamento'
-             u' para NF-e e NFC-e. Para as notas com finalidade de Ajuste'
-             u' ou Devolução o campo Forma de Pagamento deve ser preenchido'
-             u' com 90 - Sem Pagamento.'
-    )
 
     @api.one
     @api.constrains('number')
