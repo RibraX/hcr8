a7b065b10b35e69956a600dfb501073535807a8b
ribrax <joaobraz@ribrax.com.br>
2019-05-13 14:42:57 -0300

    tpag

--- l10n-brazil/l10n_br_account_product/models/account.py
+++ l10n-brazil/l10n_br_account_product/models/account.py
@@ -13,27 +13,7 @@ class AccountPaymentTerm(models.Model):
 
     indPag = fields.Selection(
         [('0', u'Pagamento à Vista'), ('1', u'Pagamento à Prazo'),
-         ('2', 'Outros')], 'Forma de Pagamento', default='1')
-    tPag = fields.Selection([
-            ('01', u'01 - Dinheiro'),
-            ('02', u'02 - Cheque'),
-            ('03', u'03 - Cartão de Crédito'),
-            ('04', u'04 - Cartão de Débito'),
-            ('06', u'05 - Crédito Loja'),
-            ('10', u'10 - Vale Alimentação'),
-            ('11', u'11 - Vale Refeição'),
-            ('12', u'12 - Vale Presente'),
-            ('13', u'13 - Vale Combustível'),
-            ('15', u'15 - Boleto Bancário'),
-            ('90', u'90 - Sem pagamento'),
-            ('99', u'99 - Outros')
-        ], string='Tipo de Pagamento da NF', required=True,
-            help=u'Obrigatório o preenchimento do Grupo Informações de Pagamento'
-                u' para NF-e e NFC-e. Para as notas com finalidade de Ajuste'
-                u' ou Devolução o campo Forma de Pagamento deve ser preenchido'
-                u' com 90 - Sem Pagamento.'
-        )         
-
+         ('2', 'Outros')], 'Forma de Pagamento', default='1')      
 
 class AccountTaxTemplate(models.Model):
     """Implement computation method in taxes"""
